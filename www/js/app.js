4c705552aee3b71ad5981cd6fe5122a257e057dd
suntorytime <stevensm90@gmail.com>
2016-03-15 16:46:36 -0700

    clean up comments

--- www/js/app.js
+++ www/js/app.js
@@ -12,10 +12,6 @@ angular.module('starter', ['ionic',
   'angular-storage',
   'angular-jwt', 'ngStorage', 'chart.js'])
 
-// .constant('ApiEndpoint', {
-//   url: 'http://localhost:8100/api'
-// })
-
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -71,35 +67,6 @@ angular.module('starter', ['ionic',
       }
     }
   })
-
-  // .state('tab.chats', {
-  //     url: '/chats',
-  //     views: {
-  //       'tab-chats': {
-  //         templateUrl: 'templates/tab-chats.html',
-  //         controller: 'ChatsCtrl'
-  //       }
-  //     }
-  //   })
-  //   .state('tab.chat-detail', {
-  //     url: '/chats/:chatId',
-  //     views: {
-  //       'tab-chats': {
-  //         templateUrl: 'templates/chat-detail.html',
-  //         controller: 'ChatDetailCtrl'
-  //       }
-  //     }
-  //   })
-
-  // .state('tab.account', {
-  //   url: '/account',
-  //   views: {
-  //     'tab-account': {
-  //       templateUrl: 'templates/tab-account.html',
-  //       controller: 'AccountCtrl'
-  //     }
-  //   }
-  // });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
